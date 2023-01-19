7af2f87e4eb3ec885d9e69094293837167b41df7
Sam Selikoff <sam.selikoff@gmail.com>
2023-01-19 17:24:53 -0500

    wip

--- app-dir/app/animated-outlet.tsx
+++ app-dir/app/animated-outlet.tsx
@@ -14,7 +14,7 @@ export default function AnimatedOutlet({ children }: any) {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 2, type: "spring", bounce: 0 }}
+        transition={{ duration: 1 }}
       >
         {children}
       </motion.div>
